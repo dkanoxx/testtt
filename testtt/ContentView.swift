2b8852b11f6e235e2614e4814842c65a6bd0cc4d
dkanoxx <sepulvedafernando04@gmail.com>
2020-03-19 21:17:47 -0400

    this is just temporary okdsvsdbsab

--- testtt/ContentView.swift
+++ testtt/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+            }
+            Text("Hello, World! this is my ")
+        }
+      
     }
 }
 
