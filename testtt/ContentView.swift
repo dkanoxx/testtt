4b753ddb0b06838ad4fbb91dbc6a79ad01bec418
dkanoxx <sepulvedafernando04@gmail.com>
2020-03-19 21:10:57 -0400

    Initial Commit

---
+++ testtt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testtt
+//
+//  Created by fernando sepulveda on 3/19/20.
+//  Copyright © 2020 fernando sepulveda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
